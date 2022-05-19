0c47251f0d5da993d0112182f75e74e1ad452979
LancerComet <chw644@hotmail.com>
2022-05-20 04:32:59 +0800

    + Update async component testing.

--- test/async-comp.spec.tsx
+++ test/async-comp.spec.tsx
@@ -1,36 +1,27 @@
 /* eslint-disable no-undef */
 
-import Vue from 'vue'
-import { defineComponent } from '@vue/composition-api'
+import { defineAsyncComponent, defineComponent } from '@vue/composition-api'
 import { mount } from '@vue/test-utils'
 import { sleep } from './utils/sleep'
 
-const getComponents = async () => {
-  await sleep(100)
-
-  const Example = defineComponent({
-    setup () {
-      return () => (
-        <span>Greeting!</span>
-      )
-    }
+it('Async component should work properly.', async () => {
+  const Example = defineAsyncComponent(async () => {
+    await sleep(10)
+    return defineComponent({
+      setup () {
+        return () => (
+          <span>Greeting!</span>
+        )
+      }
+    })
   })
 
   const Example2: any = {
-    render: h => h('div', ['Wow', 'Such a div!'])
+    components: {
+      Example
+    },
+    render: h => h('Example')
   }
-
-  const Example3 = Vue.extend({
-    render: h => h('div', ['Example 3'])
-  })
-
-  return {
-    Example, Example2, Example3
-  }
-}
-
-it('Async component should work properly.', async () => {
-  const { Example, Example2, Example3 } = await getComponents()
 
   const Wrapper = defineComponent({
     name: 'Wrapper',
@@ -38,20 +29,17 @@ it('Async component should work properly.', async () => {
       return () => (
         <div>
           <h1>Title</h1>
-          <Example />
           <Example2 />
-          <Example3 />
         </div>
       )
     }
   })
 
   const wrapper = mount(Wrapper)
+  await sleep(20)
   expect(wrapper.html()).toBe(
     '<div>\n' +
     '  <h1>Title</h1><span>Greeting!</span>\n' +
-    '  <div>WowSuch a div!</div>\n' +
-    '  <div>Example 3</div>\n' +
     '</div>'
   )
 })
